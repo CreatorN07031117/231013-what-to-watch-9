1fcfa3e04e85e46f63a9f4e8fd543746248e4a0b
marishaalis@rambler.ru <marishaalis@rambler.ru>
2022-02-14 18:45:09 +0700

    Добавляет компоненты

---
+++ project/src/components/film-card/film-card.tsx
@@ -0,0 +1,19 @@
+type FilmCardProps = {
+  filmTitle: string;
+  posterImage: string
+}
+
+function FilmCard({filmTitle, posterImage}: FilmCardProps): JSX.Element {
+  return (
+    <article className="small-film-card catalog__films-card">
+      <div className="small-film-card__image">
+        <img src={posterImage} alt={filmTitle} width="280" height="175" />
+      </div>
+      <h3 className="small-film-card__title">
+        <a className="small-film-card__link" href="film-page.html">{filmTitle}</a>
+      </h3>
+    </article>
+  );
+}
+
+export default FilmCard;
---
+++ project/src/components/my-list/my-list.tsx
@@ -0,0 +1,46 @@
+import FilmCard from '../film-card/film-card';
+import FilmsList from '../mock-film-list/mock-films-list';
+import Footer from '../footer/footer';
+
+function MyList(): JSX.Element {
+  return (
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <div className="logo">
+          <a href="main.html" className="logo__link">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
+        </div>
+
+        <h1 className="page-title user-page__title">My list</h1>
+
+        <ul className="user-block">
+          <li className="user-block__item">
+            <div className="user-block__avatar">
+              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+            </div>
+          </li>
+          <li className="user-block__item">
+            <a className="user-block__link" href="# ">Sign out</a>
+          </li>
+        </ul>
+      </header>
+
+      <section className="catalog">
+        <h2 className="catalog__title visually-hidden">Catalog</h2>
+
+        <div className="catalog__films-list">
+          {
+            FilmsList.slice(0, 5).map((item) => <FilmCard key={item.filmTitle} filmTitle={item.filmTitle} posterImage={item.posterImage} />)
+          }
+        </div>
+      </section>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default MyList;
